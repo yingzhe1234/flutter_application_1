9c15d4f331e2b9d14f6ab9372b11d87e96b1a09e
yingzhe1234 <chenyingche0802@gmail.com>
2023-07-28 02:31:47 +0800

    First commit

--- lib/view_models/meal_provider.dart
+++ lib/view_models/meal_provider.dart
@@ -3,46 +3,67 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import '../models/meal.dart';
-import '../models/meal_list.dart';
+
+// import 'meal_model.dart';
 
 class MealProvider with ChangeNotifier {
-  late MealList mealList;
+  List<Meal> _meals = [];
 
-  MealListViewModel() {
-    fetchData();
+  List<Meal> get meals => _meals;
+
+  MealProvider() {
+    fetchMeals();
   }
 
-  Future<void> fetchData() async {
-    // replace the URL with the actual API endpoint
-    var response = await http.get(Uri.parse(
-        'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian'));
+  Meal? findMealById(String id) {
+    return _meals.firstWhere((meal) => meal.idMeal == id, orElse: () {
+      throw Exception('Meal with ID: $id not found');
+    });
+  }
 
-    if (response.statusCode == 200) {
-      String content = response.body;
-      Map<String, dynamic> mealJson = jsonDecode(content);
-      mealList = MealList.fromJson(mealJson);
-      notifyListeners(); // notify any widgets listening to this model that something has changed
-    } else {
-      // You might want to throw an exception here, or handle the error in some way
+  Future<void> fetchMeals() async {
+    try {
+      var url = Uri.parse(
+          'https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert');
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        var mealsData = data['meals'] as List;
+        _meals = mealsData.map((e) => Meal.fromJson(e)).toList();
+
+        for (int i = 0; i < _meals.length; i++) {
+          var detailUrl = Uri.parse(
+              'https://themealdb.com/api/json/v1/1/lookup.php?i=${_meals[i].idMeal}');
+          var detailResponse = await http.get(detailUrl);
+          if (detailResponse.statusCode == 200) {
+            var detailData = jsonDecode(detailResponse.body);
+            var mealDetailData = detailData['meals']
+                [0]; // assuming the API gives a list with a single meal detail
+            _meals[i] = Meal.fromJson(
+                mealDetailData); // replace the meal with a new one that includes details
+          }
+        }
+        notifyListeners();
+      } else {
+        throw Exception('Failed to load meals');
+      }
+    } catch (e) {
+      print('Error: $e');
+      // You can show an error message to the user or do something else
     }
   }
 
-  // List<Meal> _meals = [];
+  Future<Meal> fetchMealDetails(String idMeal) async {
+    var url = Uri.parse(
+        'https://www.themealdb.com/api/json/v1/1/lookup.php?i=$idMeal');
+    var response = await http.get(url);
 
-  // List<Meal> get meals => _meals;
-
-  // Future<void> fetchMeals() async {
-  //   var url = Uri.parse(
-  //       'https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert');
-  //   var response = await http.get(url);
-
-  //   if (response.statusCode == 200) {
-  //     var data = jsonDecode(response.body);
-  //     var mealsData = data['meals'] as List;
-  //     _meals = mealsData.map((e) => Meal.fromJson(e)).toList();
-  //     notifyListeners();
-  //   } else {
-  //     throw Exception('Failed to load meals');
-  //   }
-  // }
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      return Meal.fromJson(data['meals'][0]);
+    } else {
+      throw Exception('Failed to load meal details');
+    }
+  }
 }
