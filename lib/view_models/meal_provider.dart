d12c9e4e6b8f1014b97f27cbb806fd3ffc914de2
yingzhe1234 <chenyingche0802@gmail.com>
2023-07-29 04:25:23 +0800

    change the way to load meal detail data

--- lib/view_models/meal_provider.dart
+++ lib/view_models/meal_provider.dart
@@ -31,19 +31,6 @@ class MealProvider with ChangeNotifier {
         var data = jsonDecode(response.body);
         var mealsData = data['meals'] as List;
         _meals = mealsData.map((e) => Meal.fromJson(e)).toList();
-
-        for (int i = 0; i < _meals.length; i++) {
-          var detailUrl = Uri.parse(
-              'https://themealdb.com/api/json/v1/1/lookup.php?i=${_meals[i].idMeal}');
-          var detailResponse = await http.get(detailUrl);
-          if (detailResponse.statusCode == 200) {
-            var detailData = jsonDecode(detailResponse.body);
-            var mealDetailData = detailData['meals']
-                [0]; // assuming the API gives a list with a single meal detail
-            _meals[i] = Meal.fromJson(
-                mealDetailData); // replace the meal with a new one that includes details
-          }
-        }
         notifyListeners();
       } else {
         throw Exception('Failed to load meals');
--- lib/views/meal_detail_screen.dart
+++ lib/views/meal_detail_screen.dart
@@ -1,43 +1,109 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/view_models/meal_provider.dart';
 import 'package:provider/provider.dart';
-
+import '../view_models/meal_provider.dart';
 import '../models/meal.dart';
 
-class MealDetailScreen extends StatelessWidget {
+class MealDetailScreen extends StatefulWidget {
   final String mealId;
 
   MealDetailScreen({Key? key, required this.mealId}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    var mealProvider = Provider.of<MealProvider>(context);
-    final meal = mealProvider.findMealById(mealId);
+  _MealDetailScreenState createState() => _MealDetailScreenState();
+}
 
+class _MealDetailScreenState extends State<MealDetailScreen> {
+  Meal? meal;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDetails();
+  }
+
+  void fetchDetails() async {
+    try {
+      meal = await Provider.of<MealProvider>(context, listen: false)
+          .fetchMealDetails(widget.mealId);
+      setState(() {});
+    } catch (e) {
+      // handle error
+      print("Fetch meal detail data error");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meal Detail'),
+        title: Text(meal?.mealName ?? 'Loading...'),
       ),
       body: meal != null
-          ? Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(meal.mealName,
-                      style: Theme.of(context).textTheme.headline6),
-                  SizedBox(height: 8),
-                  Image.network(meal.mealThumb),
-                  SizedBox(height: 8),
-                  Text('Instructions: ${meal.mealInstructions}'),
-                  SizedBox(height: 8),
-                  Text('Category: ${meal.mealCategory}'),
-                  SizedBox(height: 8),
-                  Text('Area: ${meal.mealArea}'),
-                ],
-              ),
+          ? ListView(
+              children: <Widget>[
+                if (meal!.mealThumb != null) Image.network(meal!.mealThumb!),
+                ListTile(
+                  title: Text('Instructions'),
+                  subtitle: Text(
+                      meal?.mealInstructions ?? 'No instructions provided'),
+                ),
+                ListTile(
+                  title: Text('Category'),
+                  subtitle: Text(meal?.mealCategory ?? 'No category provided'),
+                ),
+                ListTile(
+                  title: Text('Area'),
+                  subtitle: Text(meal?.mealArea ?? 'No area provided'),
+                ),
+              ],
             )
-          : Center(child: Text('Loading...')),
+          : Center(child: CircularProgressIndicator()),
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/view_models/meal_provider.dart';
+// import 'package:provider/provider.dart';
+
+// import '../models/meal.dart';
+
+// class MealDetailScreen extends StatelessWidget {
+//   final String mealId;
+
+//   MealDetailScreen({Key? key, required this.mealId}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var mealProvider = Provider.of<MealProvider>(context);
+//     final meal = mealProvider.findMealById(mealId);
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Meal Detail'),
+//       ),
+//       body: meal != null
+//           ? Padding(
+//               padding: EdgeInsets.all(16),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: <Widget>[
+//                   Text(meal.mealName,
+//                       style: Theme.of(context).textTheme.headline6),
+//                   SizedBox(height: 8),
+//                   Image.network(meal.mealThumb),
+//                   SizedBox(height: 8),
+//                   Text('Instructions: ${meal.mealInstructions}'),
+//                   SizedBox(height: 8),
+//                   Text('Category: ${meal.mealCategory}'),
+//                   SizedBox(height: 8),
+//                   Text('Area: ${meal.mealArea}'),
+//                 ],
+//               ),
+//             )
+//           : Center(child: Text('Loading...')),
+//     );
+//   }
+// }
